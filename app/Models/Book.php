050f287c4c9fda0453f77d23f61cbcc573e1936d
Ronan Campos <ronannc1@yahoo.com.br>
2023-01-15 14:04:37 -0300

    populando banco fake

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    protected $hidden = [
+      'created_at',
+      'updated_at'
+    ];
+    protected $fillable = [
+        'name',
+        'isbn',
+        'value'
+    ];
 }
